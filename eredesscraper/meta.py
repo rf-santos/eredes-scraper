d2c5169c42c6f256eb34c4975c7fb82867707553
Ricardo Filipe dos Santos <ricardofilipecdsantos@gmail.com>
2023-10-31 15:59:06 +0000

    included CLI version check and other small fixes

---
+++ eredesscraper/meta.py
@@ -0,0 +1,22 @@
+import typer
+import tomllib
+from pathlib import Path
+
+from eredesscraper._version import get_version
+
+# import project info from pyproject.toml
+project = tomllib.loads(Path(Path(__file__).parent.parent / "pyproject.toml").resolve().read_text())
+
+cli_header = f"""
+{typer.style("E-REDES Scraper", 
+             fg=typer.colors.BRIGHT_CYAN, 
+             bold=True)}
+{typer.style(f"Version: {get_version()}", 
+             bold=True)}
+{typer.style(f"Authors: {project['tool']['poetry']['authors']}", 
+             bold=True)}
+{typer.style(f"Hompage: {typer.style(project['tool']['poetry']['homepage'],fg=typer.colors.CYAN, italic=True)}", 
+             bold=True)}
+
+{project['tool']['poetry']['description']}
+"""
