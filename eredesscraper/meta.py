c8962a6940a9f1fdd433c3ba71957a1a6b78ec2d
Ricardo Filipe dos Santos <ricardofilipecdsantos@gmail.com>
2024-06-11 13:35:40 +0100

    chore: Update supported_databases and user_agent_list in meta.py

--- eredesscraper/meta.py
+++ eredesscraper/meta.py
@@ -23,3 +23,26 @@ cli_header = f"""
 supported_workflows = ["current", "previous", "select"]
 
 supported_databases = ["influxdb"]
+
+user_agent_list = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.2535.92',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+]
+
+en_pt_month_map = {
+    "01": "jan",
+    "02": "fev",
+    "03": "mar",
+    "04": "abr",
+    "05": "mai",
+    "06": "jun",
+    "07": "jul",
+    "08": "ago",
+    "09": "set",
+    "10": "out",
+    "11": "nov",
+    "12": "dez",
+}
